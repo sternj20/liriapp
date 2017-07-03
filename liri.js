c97295607cc6f7edeac2b8de84df688b1ba66217
sternj20 <jessesternmusic@gmail.com>
2017-07-03 11:03:11 -0700

    logging tweets in the console

--- liri.js
+++ liri.js
@@ -1,2 +1,25 @@
 var keys = require("./keys.js");
-console.log(keys)
+var consumerKey = keys.twitterKeys.consumer_key;
+var consumerSecret = keys.twitterKeys.consumer_secret;
+var accessTokenKey = keys.twitterKeys.access_token_key;
+var accessTokenSecret = keys.twitterKeys.access_token_secret;
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: consumerKey,
+  consumer_secret: consumerSecret,
+  access_token_key: accessTokenKey,
+  access_token_secret: accessTokenSecret
+});
+
+var params = {screen_name: 'jessesternmusic'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+  	tweets.forEach(function(element) {
+  		console.log(element.created_at);
+  		console.log(element.text);	
+  	});
+    // console.log(tweets[0].text);
+  }
+});
