c8d6ad04375124b988de0987a7836f6eb37b64bd
sternj20 <jessesternmusic@gmail.com>
2017-07-03 17:47:00 -0700

    added spotify functionality

--- keys.js
+++ keys.js
@@ -1,8 +1,13 @@
-console.log('this is loaded');
+// console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'vXy1LUdT9S33t41cddUhmMQR2',
   consumer_secret: '212BGWfcAL6MHJFb5HkJmuD9ruHb8t1U1wNle0uOTvHBZDfrkg',
   access_token_key: '18205829-0uASXHRqVDTSnMZ7Ans6bjD9gBR80a1pWc5d0GHAi',
   access_token_secret: '3PFSziiSJhsZ217RPluOyPSwnBPXbHvtzC4xultEiZPOB',
+};
+
+exports.spotifyKeys = {
+	client_id: '00f58634f2c143b68600f2474f2f22c6',
+	client_secret: '42a1a296e9934c50801b937449da21e7'
 };
--- liri.js
+++ liri.js
@@ -1,25 +1,80 @@
 var keys = require("./keys.js");
-var consumerKey = keys.twitterKeys.consumer_key;
-var consumerSecret = keys.twitterKeys.consumer_secret;
-var accessTokenKey = keys.twitterKeys.access_token_key;
-var accessTokenSecret = keys.twitterKeys.access_token_secret;
 
+//Twitter Keys
+var twitterConsumerKey = keys.twitterKeys.consumer_key;
+var twitterConsumerSecret = keys.twitterKeys.consumer_secret;
+var twitterAccessTokenKey = keys.twitterKeys.access_token_key;
+var twitterAccessTokenSecret = keys.twitterKeys.access_token_secret;
+
+//Command input given by user in terminal
+var command = process.argv[2];
+
+
+//Twitter
 var Twitter = require('twitter');
- 
+
 var client = new Twitter({
-  consumer_key: consumerKey,
-  consumer_secret: consumerSecret,
-  access_token_key: accessTokenKey,
-  access_token_secret: accessTokenSecret
+	consumer_key: twitterConsumerKey,
+	consumer_secret: twitterConsumerSecret,
+	access_token_key: twitterAccessTokenKey,
+	access_token_secret: twitterAccessTokenSecret
 });
 
 var params = {screen_name: 'jessesternmusic'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  	tweets.forEach(function(element) {
-  		console.log(element.created_at);
-  		console.log(element.text);	
-  	});
-    // console.log(tweets[0].text);
-  }
-});
+
+
+
+//Spotify
+
+var Spotify = require('node-spotify-api');
+
+var spotifyClientId = keys.spotifyKeys.client_id;
+var spotifyClientSecret = keys.spotifyKeys.client_secret;
+
+
+//Functions
+function showTweets(){
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+			tweets.forEach(function(element, index) {
+				index = index + 1;
+				if (index <= 20){
+					console.log(index);
+					console.log(element.created_at);
+					console.log(element.text);	
+				}
+			});
+		}
+	});
+}
+
+function showSpotifySong(){
+
+	var songToFind = process.argv[3];
+	var spotify = new Spotify({
+		id: spotifyClientId,
+		secret: spotifyClientSecret
+	});
+
+
+	spotify.search({ type: 'track', query: songToFind}, function(err, data) {
+		if (err) {
+			return console.log('Error occurred: ' + err);
+		}
+			console.log(data.tracks.items[0].album.artists[0].name);
+			console.log(data.tracks.items[0].name);
+			console.log(data.tracks.items[0].preview_url);
+			console.log(data.tracks.items[0].album.name);
+	});
+}
+
+
+if(command === 'my-tweets'){
+	showTweets();
+} else if (command === 'spotify-this-song'){
+	showSpotifySong();
+} else if (command === 'movie-this'){
+
+} else if (command === 'do-what-it-says'){
+
+}
